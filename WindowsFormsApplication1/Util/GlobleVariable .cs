a8807cc66a19062443bae5bed76286395998dc14
yigehui <351591801@qq.com>
2018-05-22 17:43:01 +0800

    新增的全局类 定义了相似度

---
+++ WindowsFormsApplication1/Util/GlobleVariable .cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApplication1.Util
+{
+   public class GlobleVariable
+    {
+        //判断相似度的差值
+        public static double chazhi = 0.05;
+    }
+}
